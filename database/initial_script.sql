3cdbc94485a550896a3a06c9fa8bc73118e6fa74
Rafał Kowalski <rafkow91@gmail.com>
2021-10-12 20:48:59 +0200

    Add database's configuration

---
+++ database/initial_script.sql
@@ -0,0 +1,43 @@
+-- create table titles
+create table titles
+(
+    id serial primary key,
+    title_name varchar(255) unique,
+    basic_salary int
+);
+
+-- create table employees
+create table employees
+(
+    id serial primary key,
+    first_name varchar(30),
+    last_name varchar(30),
+    title_id int,
+    mail varchar(255),
+    phone int,
+    login varchar(255),
+    password varchar(255)
+);
+
+-- create table worktime
+create table worktime
+(
+    employee_id int,
+    start_time timestamp,
+    end_time timestamp
+);
+
+select * from titles;
+select * from employees;
+select * from worktime;
+
+select * from job_service.worktime;
+
+
+-- create table worktime
+create table job_service.worktime
+(
+    employee_id int,
+    start_time timestamp,
+    end_time timestamp
+);
