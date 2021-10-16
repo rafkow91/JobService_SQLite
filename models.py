4537e3c4fa326f9c4d39a0d4b248cedd35de85b4
Rafał Kowalski <rafkow91@gmail.com>
2021-10-17 00:08:09 +0200

    add model of employee

---
+++ models.py
@@ -0,0 +1,12 @@
+from views.abstract_view import AbstractView
+
+
+class Employee(AbstractView):
+    def add():
+        return 'add'
+
+    def edit():
+        return 'edit'
+
+    def index():
+        return 'index'
