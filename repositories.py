7f7ba2a1ff8c4c1b29b555e0ace5fddcb026e31e
Rafał Kowalski <rafkow91@gmail.com>
2021-10-29 20:02:43 +0200

    add input end worktime and time validation

--- repositories.py
+++ repositories.py
@@ -10,6 +10,19 @@ class WorktimeRepository:
     def __init__(self, employee_id: int) -> None:
         self.employee_id = employee_id
 
+    def add_workday(self, worktime_date: date):
+        connection = connect(DB_URL)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''
+                INSERT INTO worktime (`employee_id`, `workday`)
+                VALUES (?, ?)
+            ''',
+            (self.employee_id, worktime_date)
+        )
+
+        connection.commit()
+
     def get_workday(self, worktime_date: date):
         connection = connect(DB_URL)
         cursor = connection.cursor()
@@ -24,20 +37,7 @@ class WorktimeRepository:
 
         return cursor.fetchone()
 
-    def add_workday(self, worktime_date: date):
-        connection = connect(DB_URL)
-        cursor = connection.cursor()
-        cursor.execute(
-            '''
-                INSERT INTO worktime (`employee_id`, `workday`)
-                VALUES (?, ?)
-            ''',
-            (self.employee_id, worktime_date)
-        )
-
-        connection.commit()
-
-    def add_start_time(self, worktime_date: date, start_time: time):
+    def add_start_time(self, worktime_date: date, time: time):
         connection = connect(DB_URL)
         cursor = connection.cursor()
         cursor.execute(
@@ -46,7 +46,49 @@ class WorktimeRepository:
                 SET `start_time` = ?
                 WHERE `employee_id` = ? AND `workday` = ?
             ''',
-            (start_time, self.employee_id, worktime_date)
+            (time, self.employee_id, worktime_date)
         )
 
         connection.commit()
+
+    def get_start_time(self, worktime_date: date):
+        connection = connect(DB_URL)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''
+                SELECT start_time
+                FROM worktime
+                WHERE `employee_id` = ? AND `workday` = ?
+            ''',
+            (self.employee_id, worktime_date)
+        )
+
+        return cursor.fetchone()
+
+    def add_end_time(self, worktime_date: date, time: time):
+        connection = connect(DB_URL)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''
+                UPDATE worktime
+                SET `end_time` = ?
+                WHERE `employee_id` = ? AND `workday` = ?
+            ''',
+            (time, self.employee_id, worktime_date)
+        )
+
+        connection.commit()
+
+    def get_end_time(self, worktime_date: date):
+        connection = connect(DB_URL)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''
+                SELECT end_time
+                FROM worktime
+                WHERE `employee_id` = ? AND `workday` = ?
+            ''',
+            (self.employee_id, worktime_date)
+        )
+
+        return cursor.fetchone()
--- views/worktime_managment.py
+++ views/worktime_managment.py
@@ -7,8 +7,6 @@ from views.abstract_view import AbstractView
 from repositories import WorktimeRepository
 
 
-
-
 class AddWorktime(AbstractView):
     LABEL = 'Uzupełnianie czasu pracy'
     OPTIONS = {
@@ -39,7 +37,7 @@ class AddWorktime(AbstractView):
         if option == 1:
             self.input_start_time()
         elif option == 2:
-            self.add_end_time()
+            self.input_end_time()
         elif option == 0:
             return False
 
@@ -48,10 +46,10 @@ class AddWorktime(AbstractView):
         MethodLABEL = 'Czas wejścia'
         self.draw_logo(MethodLABEL)
         repository = WorktimeRepository(self.employee_id)
-        
+
         worktime_date = None
         worktime_time = None
-        
+
         while True:
             try:
                 worktime_date = datetime.strptime(input('Data: [DD-MM-RRRR] '), '%d-%m-%Y').date()
@@ -59,15 +57,26 @@ class AddWorktime(AbstractView):
                 print('podano niepoprawną datę!!')
                 continue
             break
-        
+
+        worktime_end_time = repository.get_end_time(worktime_date)
+        if worktime_end_time is None:
+            worktime_end_time = datetime.strptime('23:59:59', '%H:%M:%S').time()
+        else:
+            worktime_end_time = datetime.strptime(worktime_end_time[0], '%H:%M:%S').time()
+
         while True:
             try:
                 worktime_time = datetime.strptime(input('Godzina wejścia [HH:MM] '), '%H:%M').time()
+                if worktime_time > worktime_end_time:
+                    print(
+                        f'Godzina zakończenia pracy jest wcześniejsza niż godzina rozpoczęcia!!\nWpisana godzina zakończenia: {worktime_end_time}')
+                    sleep(2)
+                    continue
             except:
                 print('podano niepoprawną godzinę!!')
                 continue
             break
-        
+
         worktime_time = str(worktime_time)
 
         if repository.get_workday(worktime_date) is None:
@@ -75,17 +84,47 @@ class AddWorktime(AbstractView):
 
         repository.add_start_time(worktime_date, worktime_time)
 
-
-    def add_end_time(self):
+    def input_end_time(self):
+        system('clear')
         MethodLABEL = 'Czas wyjścia'
         self.draw_logo(MethodLABEL)
         repository = WorktimeRepository(self.employee_id)
-        sleep(10)
 
+        worktime_date = None
+        worktime_time = None
 
-class CheckWorktime(AbstractView):
-    LABEL = 'Kontrola czasu pracy'
+        while True:
+            try:
+                worktime_date = datetime.strptime(input('Data: [DD-MM-RRRR] '), '%d-%m-%Y').date()
+            except:
+                print('podano niepoprawną datę!!')
+                continue
+            break
 
-    def draw(self):
-        self.draw_logo(CheckWorktime.LABEL)
-        repository = WorktimeRepository(self.employee_id)
+        worktime_start_time = repository.get_start_time(worktime_date)
+        if worktime_start_time is None:
+            worktime_start_time = datetime.strptime('00:00:00', '%H:%M:%S').time()
+        else:
+            worktime_start_time = datetime.strptime(worktime_start_time[0], '%H:%M:%S').time()
+
+        print(worktime_start_time)
+
+        while True:
+            try:
+                worktime_time = datetime.strptime(input('Godzina wejścia [HH:MM] '), '%H:%M').time()
+                if worktime_time < worktime_start_time:
+                    print(
+                        f'Godzina rozpoczęcia pracy jest późniejsza niż godzina zakończenia!!\nWpisana godzina rozpoczęcia: {worktime_start_time}')
+                    sleep(2)
+                    continue
+            except:
+                print('podano niepoprawną godzinę!!')
+                continue
+            break
+
+        worktime_time = str(worktime_time)
+
+        if repository.get_workday(worktime_date) is None:
+            repository.add_workday(worktime_date)
+
+        repository.add_end_time(worktime_date, worktime_time)
