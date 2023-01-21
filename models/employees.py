8106c9345803b0eca56f3c3473f22751ce86a65d
Rafał Kowalski <rafkow91@yahoo.com>
2023-01-21 23:18:30 +0100

    add new methods to employee model

--- models/employees.py
+++ models/employees.py
@@ -11,3 +11,9 @@ class Employee(BaseModel):
     mail: str
     title_id: int
     password: str
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return f'{self.id} - {self.first_name} {self.last_name} ({self.title_id})'
